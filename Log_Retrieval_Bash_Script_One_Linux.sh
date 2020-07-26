b15ba0456becfeb5d07dab3114ae27d871b675ec
Chad <github@chad.org>
2020-07-26 13:41:56 -0400

    Use language-specific Desktop/ dir

--- Log_Retrieval_Bash_Script_One_Linux.sh
+++ Log_Retrieval_Bash_Script_One_Linux.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
+set -ue
+
+DESKTOP=$(xdg-user-dir DESKTOP || echo ~/Desktop)   # language specific, so ask system, with fallback
 echo Creating Folder for Logs...
-cd ~/Desktop
-mkdir SpiderOak_One_Log_Files
+mkdir -p "${DESKTOP}/SpiderOak_One_Log_Files"
+
 echo Now gathering logs...
-cd ~/.config/SpiderOakONE
-cp *.log ~/Desktop/SpiderOak_One_Log_Files
+cp -v ~/.config/SpiderOakONE/*.log "$DESKTOP/SpiderOak_One_Log_Files/"
 echo Log retrieval complete. Compressing...
-cd ~/Desktop
+
+cd "$DESKTOP"
 zip -r SpiderOak_One_Log_Files.zip SpiderOak_One_Log_Files
 rm -R SpiderOak_One_Log_Files
 echo Compression complete. Have a nice day!
